84547bf7cd94f6545384bf816bec32173b20a485
Dave Cornejo <dave@dogwood.com>
2016-07-08 12:19:32 -1000

    fix license

--- obdtest.js
+++ obdtest.js
@@ -2,12 +2,20 @@
  * Created by dave on 7/8/16.
  */
 /*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <dave@dogwood.com> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return Dave Cornejo
- * ----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * (C) Copyright 2013, TNO
+ * Author: Eric Smekens
  */
 
 var obdReader = require('bluetooth-obd');
@@ -36,5 +44,5 @@ ObdReader.on('debug', function (data) {
 });
 
 // Use first device with 'obd' in the name
-ObdReader.autoconnect('obd');
+ObdReader.autoconnect('OBD');
 
