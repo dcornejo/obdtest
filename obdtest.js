43165cb3c556e9483f46e4b6fdc5fb658aafc2fd
Dave Cornejo <dave@dogwood.com>
2017-03-07 12:23:54 -0500

    rewrite for serial obd, add in debugging alternatives

--- obdtest.js
+++ obdtest.js
@@ -1,48 +1,104 @@
-/**
- * Created by dave on 7/8/16.
- */
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * (C) Copyright 2013, TNO
- * Author: Eric Smekens
- */
+#!/usr/bin/env node
 
-var obdReader = require('bluetooth-obd');
+//
+// DCS Zephyr BioHarness 3 Data Source
+//
 
-var ObdReader = new obdReader();
+"use strict";
 
-ObdReader.on('dataReceived', function (data) {
-    var currentDate = new Date();
-    console.log(currentDate.getTime(), data);
-    console.log(data);
+let serialObd2Device = false;
+let debugMe = false;
+
+// =======================================
+
+let getOpt = require('node-getopt');
+const fs = require('fs');
+
+let getopt = new getOpt([
+    ['i', 'interface=', 'specify device interface'],
+    ['d', 'debug', 'debug mode'],
+    ['h', 'help', 'display this help']
+]);
+
+getopt.setHelp(
+    "Usage: node index.js [OPTION]\n\n" + "[[OPTIONS]]\n"
+);
+
+let cmdOptions = getopt.parseSystem(process.argv.slice(2));
+
+if (cmdOptions.options.help) {
+    getopt.showHelp();
+    process.exit();
+}
+
+if (cmdOptions.options.debug) {
+    debugMe = true;
+}
+
+if (cmdOptions.options.interface) {
+    serialObd2Device = fs.realpathSync(cmdOptions.options.interface);
+}
+else {
+    console.log('must specify an interface');
+    process.exit();
+}
+
+// =======================================
+
+// Serial/USB OBD-II READER
+
+let OBDReader = require('serial-obd');
+
+let serialOBDReader = new OBDReader(serialObd2Device, {
+    baudrate: 115200
 });
 
-ObdReader.on('connected', function () {
+function addX(x, pid) {
+    x.addPoller(pid);
+}
+
+serialOBDReader.on('connected', function () {
+
+    // ALTERNATE 1
     this.addPoller("vss");
-    this.addPoller("rpm");
+    this.startPolling(1000); //Request all values each second.
+    setTimeout(addX(this, "rpm"), 125);
+    setTimeout(addX(this, "load_pct"), 250);
+    // END ALTERNATE 1
 
-    this.startPolling(1000);
+    // ALTERNATE 2
+    // this.addPoller("vss");
+    // this.addPoller("rpm");
+    // this.addPoller("load_pct");
+    // this.startPolling(1000);
+    // END ALTERNATE 2
+
+    // ALTERNATE 3
+    // this.addPoller("vss");
+    // this.addPoller("rpm");
+    // this.addPoller("load_pct");
+    // this.startPolling(2000);
+    // END ALTERNATE 3
+
+
 });
 
-ObdReader.on('error', function (err) {
-    console.log('ObdReader Error: ' + err);
-});
+serialOBDReader.on('dataReceived', function (data) {
+    let ts = new Date().valueOf();
 
-ObdReader.on('debug', function (data) {
-    console.log('ObdReader Debug: ' + data);
+    console.log(ts, JSON.stringify(data));
+    // alternate to 1 line above:
+    // console.log(ts);
+    // console.dir(data);
+    // end alternate
 });
 
 // Use first device with 'obd' in the name
-ObdReader.autoconnect('OBD');
+try {
+    serialOBDReader.connect(serialObd2Device);
+}
+catch (err) {
+    console.log('OBD-II connect: ' + err.context);
+}
 
+console.log('running');
